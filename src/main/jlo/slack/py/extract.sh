37fc0335f3e1a4d3eb8b5e90fe464ef931f5d7ea
Jason Lowe <jlowe000@gmail.com>
2020-04-14 20:41:08 +1000

    augmented this script to align to the new zip structure (raw data, metrics and master dataset).

--- src/main/jlo/slack/py/extract.sh
+++ src/main/jlo/slack/py/extract.sh
@@ -7,7 +7,10 @@ archive=${archive%?}
 echo "batch id is :${archive}"
 ./run.sh transform_load.py ${archive}
 cd data
-zip -r ausinnovation-slack-${archive}-${current_version}.zip ${archive}
-# rm current-dataset
-# ln -s ${archive} current-dataset
+zip -r ausinnovation-slack-${archive}-data-${current_version}.zip ${archive}/api ${archive}/files ${archive}/json ${archive}/metadata
+zip -r ausinnovation-slack-${archive}-metrics-${current_version}.zip ${archive}/csv ${archive}/metrics ${archive}/metadata
+cd ..
+./run.sh transform_load.py
+cd data
+zip -r ausinnovation-slack-${archive}-master-metrics-${current_version}.zip master metrics
 cd ..
